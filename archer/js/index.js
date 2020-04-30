cb62ca421b6ac06b733e9653841f8e2d633384ac
苍耳 <1459829778@qq.com>
2020-04-30 10:06:20 +0800

    ni

---
+++ archer/js/index.js
@@ -0,0 +1,33 @@
+$(function () {
+    banner();
+    
+});
+function banner() {
+    
+    var isMobile = false;
+    var width = $(window).width();
+    if (width <= 768) {
+        isMobile = true;
+    }
+
+    // 通过json获取
+    // ajax异步获取
+    var myData = new Array();
+    myData[0] = { pc: "./images/slide_01_2000x410.jpg", mb: "./images/slide_01_640x340.jpg" };
+    myData[1] = { pc: "./images/slide_02_2000x410.jpg", mb: "./images/slide_02_640x340.jpg" };
+    myData[2] = { pc: "./images/slide_03_2000x410.jpg", mb: "./images/slide_03_640x340.jpg" };
+    myData[3] = { pc: "./images/slide_04_2000x410.jpg", mb: "./images/slide_04_640x340.jpg" };
+    
+    var templatePoint = _.template($("#template_point").html());
+    var templateImage = _.template($("#template_image").html());
+    var htmlPoint = templatePoint({ model: myData }); 
+    var htmlImage = templateImage({ model: myData, isMobile: isMobile });
+    
+    $(".carousel-indicators").html(htmlPoint);
+    $(".carousel-inner").html(htmlImage);
+
+    $(window).on("resize",function(){
+        banner();
+    });
+
+}
