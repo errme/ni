3d33c594746030a0c8c9dd6fc93dd402b6f02e07
苍耳 <1459829778@qq.com>
2020-05-14 10:08:57 +0800

    ni

---
+++ me/nianme/js/js/fd.js
@@ -0,0 +1,18 @@
+window.onload =
+	function() {
+		var oDiv = document.getElementById("fixPara"),
+			H = 0,
+			Y = oDiv
+		while (Y) {
+			H += Y.offsetTop;
+			Y = Y.offsetParent;
+		}
+		window.onscroll = function() {
+			var s = document.body.scrollTop || document.documentElement.scrollTop
+			if (s > H) {
+				oDiv.style = "position:fixed;top:0;"
+			} else {
+				oDiv.style = ""
+			}
+		}
+	}
