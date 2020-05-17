66e29676c2616659cdb5988d2b623c3eeda0c772
苍耳 <1459829778@qq.com>
2020-05-17 10:18:31 +0800

    ni

---
+++ demo/js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+    banner();
+});
+
+function banner(){
+    var myData = new Array();
+    myData[0] = {pc:"./images/521.jpg",mb:"./images/640.png"};
+    myData[1] = {pc:"./images/521.jpg",mb:"./images/640.png"};
+    myData[2] = {pc:"./images/521.jpg",mb:"./images/640.png"};
+    myData[3] = {pc:"./images/521.jpg",mb:"./images/640.png"};
+
+    var render = function () {
+        var isMobile = false;
+        var width = $(window).width();
+        if (width <= 768) {
+            isMobile = true;
+        }
+        getData(function (data) {
+            var templatePoint = _.template($("#template_point").html());
+            var templateImage = _.template($("#template_image").html());
+            var htmlPoint = templatePoint({ model: myData });
+            var htmlImage = templateImage({ model: myData, isMobile: isMobile });
+
+            $(".carousel-indicators").html(htmlPoint);
+            $(".carousel-inner").html(htmlImage);
+        });
+
+    }
+}
