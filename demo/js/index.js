cd50e06d38dd047df723940231399d2996d7141f
苍耳 <1459829778@qq.com>
2020-05-19 10:35:48 +0800

    ni

--- demo/js/index.js
+++ demo/js/index.js
@@ -16,7 +16,7 @@ function banner() {
     var myData = new Array();
     myData[0] = { pc: "./images/banner_1.png", mb: "./images/banner_sm_1.png" };
     myData[1] = { pc: "./images/banner_2.png", mb: "./images/banner_sm_2.png" };
-    myData[2] = { pc: "./images/521.jpg", mb: "./images/banner_sm_3.png" };
+    myData[2] = { pc: "./images/banner_3.png", mb: "./images/banner_sm_3.png" };
 
     var templatePoint = _.template($("#template_point").html());
     var templateImage = _.template($("#template_image").html());
