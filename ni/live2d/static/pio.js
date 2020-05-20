728306a217915f2e42629e35dd939f9f77202b90
苍耳 <1459829778@qq.com>
2020-05-20 20:27:17 +0800

    ni

--- ni/live2d/static/pio.js
+++ ni/live2d/static/pio.js
@@ -290,5 +290,5 @@ var pio = new Paul_Pio({
         "link":["http://errr.me"]
     },
     "night": "sunMoon()",
-    "model": ["./live2d/models/snow_miku/model.json","http://errr.me/ni/live2d/models/madoka/model.json","http://errr.me/ni/live2d/models/pio/model.json"]
+    "model": ["http://errr.me/ni/live2d/models/snow_miku/model.json","http://errr.me/ni/live2d/models/madoka/model.json","http://errr.me/ni/live2d/models/pio/model.json"]
 });
